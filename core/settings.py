df62a69e68b19932b17e7452c13c7e48f23fad6d
HernanW4 <zhernandezwalter@gmail.com>
2023-11-08 21:10:02 -0500

    Changed ports

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'orca-app-nqin8.ondigitalocean.app']
 
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085', 'http://0.0.0.0:80']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085', 'http://0.0.0.0:8080']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
